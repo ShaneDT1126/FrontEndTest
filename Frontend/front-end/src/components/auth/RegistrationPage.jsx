5e848357e0f1bd75d16e55e5afc602927d37de6e
Shijuchi <shanedelatorre2412@gmail.com>
2024-05-12 15:41:42 +0800

    user management frontend

---
+++ Frontend/front-end/src/components/auth/RegistrationPage.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import UserService from '../service/UserService';
+import { useNavigate } from 'react-router-dom';
+
+function RegistrationPage() {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        username: '',
+        password: '',
+        email: '',
+        phoneNumber: '',
+        address: ''
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            // Call the register method from UserService
+
+            const token = localStorage.getItem('token');
+            await UserService.register(formData, token);
+
+            // Clear the form fields after successful registration
+            setFormData({
+                firstName: '',
+                lastName: '',
+                username: '',
+                password: '',
+                email: '',
+                phoneNumber: '',
+                address: ''
+            });
+            alert('User registered successfully');
+            navigate('/admin/user-management');
+
+        } catch (error) {
+            console.error('Error registering user:', error);
+            alert('An error occurred while registering user');
+        }
+    };
+
+    return (
+        <div className="auth-container">
+            <h2>Registration</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>First Name:</label>
+                    <input type="text" name="firstName" value={formData.firstName} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Last Name:</label>
+                    <input type="text" name="lastName" value={formData.lastName} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Username:</label>
+                    <input type="text" name="username" value={formData.username} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Password:</label>
+                    <input type="password" name="password" value={formData.password} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" name="email" value={formData.email} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Address:</label>
+                    <input type="text" name="address" value={formData.address} onChange={handleInputChange} placeholder="Enter your city" required />
+                </div>
+                <div className="form-group">
+                    <label>Phone Number:</label>
+                    <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} placeholder="Enter your city" required />
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+export default RegistrationPage;
---
+++ Frontend/front-end/src/components/userspage/ProfilePage.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react';
+import UserService from '../service/UserService';
+import { Link } from 'react-router-dom';
+
+
+
+function ProfilePage() {
+    const [profileInfo, setProfileInfo] = useState({});
+
+    useEffect(() => {
+        fetchProfileInfo();
+    }, []);
+
+    const fetchProfileInfo = async () => {
+        try {
+
+            const token = localStorage.getItem('token'); // Retrieve the token from localStorage
+            const response = await UserService.getYourProfile(token);
+            setProfileInfo(response.ourUsers);
+        } catch (error) {
+            console.error('Error fetching profile information:', error);
+        }
+    };
+
+    return (
+        <div className="profile-page-container">
+            <h2>Profile Information</h2>
+            <p>First Name: {profileInfo.firstName}</p>
+            <p>Last Name: {profileInfo.lastName}</p>
+            <p>Address: {profileInfo.address}</p>
+            <p>Phone Number: {profileInfo.phoneNumber}</p>
+            {profileInfo.role === "ADMIN" && (
+                <button><Link to={`/update-user/${profileInfo.id}`}>Update This Profile</Link></button>
+            )}
+        </div>
+    );
+}
+
+export default ProfilePage;
